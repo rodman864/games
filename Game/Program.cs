9cf211dbe5e5916faa01a70e862bf5aa074bfc74
Rodrigus Mckinney <rodrigusmckinney@gmail.com>
2021-02-05 08:59:26 -0700

    Module 11

---
+++ Game/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyGame newGame = new MyGame();
+
+            newGame.ShowMenu();
+        }
+    }
+}
+public class MyGame
+{
+    public User CurrentUser { get; set; }
+   
+
+    private Action theAttackAction;
+
+    public void ShowMenu()
+    {
+        
+        ConsoleKey key = ConsoleKey.Enter;
+
+        while(key != ConsoleKey.E)
+        {
+            Console.WriteLine("P -> Punch");
+            Console.WriteLine("S -> Attack with Sword");
+            Console.WriteLine("K -> Kick");
+            Console.WriteLine("B -> Punch and Kick");
+            Console.WriteLine("E -> Exit");
+
+
+            Console.WriteLine("");
+            Console.WriteLine("Please Select An Option: ");
+            key = Console.ReadKey().Key;
+
+            Console.WriteLine(key);
+            switch (key)
+            {
+                case ConsoleKey.P:
+                    theAttackAction = Punch;
+                    break;
+                case ConsoleKey.S:
+                    theAttackAction = AttackWithSword;
+                    break;
+                case ConsoleKey.K:
+                    theAttackAction = Kick;
+                    break;
+                case ConsoleKey.B:
+                    theAttackAction = Punch;
+                    theAttackAction += Kick;
+                    break;
+            }
+            HandleAttack();
+        }
+       
+    }
+    public void Punch()
+    {
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.WriteLine("");
+        Console.WriteLine("You have been punched");
+    }
+    public void AttackWithSword()
+    {
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("");
+        Console.WriteLine("You have been attacked with a sword");
+    }
+    public void Kick()
+    {
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("");
+        Console.WriteLine("You have been kicked");
+    }
+    
+    public void HandleAttack()
+    {
+        theAttackAction();
+        
+    }
+
+    public bool CanUserBeAttacked(User enemy)
+    {
+        if (enemy.IsGamer)
+            return true;
+        if (enemy.ClanInfo.ClanName != CurrentUser.ClanInfo.ClanName)
+            return true;
+
+        return false;
+
+    }
+}
+public class User
+{
+    public string UserName { get; set; }
+    public bool IsGamer { get; set; }
+    public Clan ClanInfo { get; set; }
+}
+public class Clan
+{
+    public string ClanName { get; set; }
+}
---
+++ MyUnitTest/UnitTest1.cs
@@ -0,0 +1,40 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CanUserBeAttacked_IfItsGamer_ReturnsTrue()
+        {
+            //Arrange
+            MyGame myGame = new MyGame();
+
+            User newUser = new User();
+            newUser.IsGamer = true;
+            newUser.UserName = "Rodrigus";
+
+            Clan newClan = new Clan();
+            newClan.ClanName = "The Best";
+            newUser.ClanInfo = newClan;
+            myGame.CurrentUser = newUser;
+
+            //Act
+            User newEnemy = new User();
+            newEnemy.IsGamer = true;
+            newEnemy.UserName = "Amos";
+
+            Clan newEnemyClan = new Clan();
+            newEnemyClan.ClanName = "SU Guys";
+            newEnemy.ClanInfo = newEnemyClan;
+
+               
+           bool canItBeAttacked =
+                myGame.CanUserBeAttacked(newEnemy);
+
+            //Assert
+            Assert.IsTrue(canItBeAttacked);
+        }
+    }
+}
